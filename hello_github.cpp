218428c1ebc55eb2613a0eccc29f7475656eee3b
ellenren <ellen.ren9@gmail.com>
2015-04-22 21:09:11 +0800

    Create hello_github.cpp

---
+++ hello_github.cpp
@@ -0,0 +1,11 @@
+#include <stdio>
+#include <stdlib>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  cout << "hellp github" << endl;
+  return 0;
+}
